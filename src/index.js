c483e0b78f0ff7be6e0825ecb9327f7d3d12d1dc
anisa707 <anisa_hajrullaj@yahoo.com>
2022-12-04 19:17:17 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react"; {/*do vendosent ne cdo file qe permban 1 react componet*/} 
+import { ReactDOM } from "react"; {/*  per te bere hook ne elementin dom ku i gjithe app react do te perfshihet*/}
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
